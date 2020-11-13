f331112a3bf437ae8b70a93b36da7d50bf9dcbc2
Sarandorm <dormjuramongkol@gmail.com>
2020-11-13 17:56:07 +0700

    Update PyMongoTest.py

--- UI/PyMongoTest.py
+++ UI/PyMongoTest.py
@@ -2,4 +2,6 @@ from pymongo import MongoClient
 client = MongoClient('localhost', 27017)
 db = client.opdms
 collection = db.underlying_disease_
-print(collection.find_one())
+x = collection.find({"underlying_disease":"N185"})
+for i in x:
+    print(i)

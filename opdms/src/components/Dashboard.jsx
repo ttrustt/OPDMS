8c79cb4607c399c75bb8df53b8a8f5891a495930
passawityakul <p.yakul@icloud.com>
2020-11-20 12:50:50 +0700

    dont conflic

--- opdms/src/components/Dashboard.jsx
+++ opdms/src/components/Dashboard.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 // import BarChart from './BarChart'
-import { Jumbotron, Container } from 'react-bootstrap'
+import { Jumbotron, Container, Button } from 'react-bootstrap'
 import Header from './Header'
 import _ from "lodash";
 // import TableComponent from './TableComponent'
@@ -8,32 +8,29 @@ import _ from "lodash";
 import ShowTable from './ShowTable'
 class DashBoard extends Component {
   //Wrap up component for Grid layout and header and all of the 'module' state being stored here from the react component map in docs
+  state = { toggleActiveUserTable:false}
   constructor(props) {
     super(props);
+    
   }
 
 
-
+  toggleShowActiveUser=()=>{
+    this.setState({toggleActiveUserTable:!this.state.toggleActiveUserTable})
+  }
   render() {
+    const fillH = { width: '100%',height:'55%' }
     return (
       <React.Fragment>
         <Header></Header>
-        {/* <DataTable data={data} columns={columns} ></DataTable> */}
-        {/* <ShowTable APIendpoint="showmedicine" payload={{user_id:'1'}}></ShowTable> */}
         <Jumbotron fluid>
           <Container>
             <h1>Welcome to OPDMS</h1>
-            <p>OPDMS stands for Out patient management system</p>
+            <p>OPDMS stands for Out Patient Management System</p>
+    <Button onClick={this.toggleShowActiveUser}>{this.state.toggleActiveUserTable?<div>Hide Active User</div>:<div>Active User</div>}</Button>
           </Container>
         </Jumbotron>
-        {/* <Container style={{marginLeft:30}}>
-           <p>Active Users in OPDMS</p>
-           </Container> */}
-          <ShowTable APIendpoint="showuser" payload={{}}></ShowTable>
-        {/* <br />
-        <div style={{ height: 300 }}>
-          <ShowTable APIendpoint="showmedicine" payload={{ user_id: 1 }}></ShowTable>
-        </div> */}
+          {this.state.toggleActiveUserTable?<div style={fillH}><ShowTable APIendpoint="showuser" payload={{}}></ShowTable></div>:<div></div>}
       </React.Fragment>
     );
   }

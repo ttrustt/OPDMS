cc3490c146ed115c398ed015b9deba70444dea55
Chi <54677538+originalchi@users.noreply.github.com>
2020-11-22 14:11:32 +0700

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -260,6 +260,21 @@ def generatereceiptnumber_endpoint():
     else: response = make_response(jsonify({'success':dbfunc_status[0],'status':dbfunc_status[1],'data':dbfunc_status[2]+1}),200)
     return response
 
+@app.route('/deleteschedule', methods=['POST'])
+@cross_origin()
+def deleteschedule_endpoint():
+    params = request.get_json()
+
+    params_list = [
+        params.get('schedule_number','null')
+    ]
+
+    dbfunc_status = deleteSchedule(params_list)
+    print(dbfunc_status)
+    if(not dbfunc_status[0]):response=make_response(jsonify({'success':dbfunc_status[0],'status':dbfunc_status[1]}),200)
+    else: response = make_response(jsonify({'success':dbfunc_status[0],'status':dbfunc_status[1]}),200)
+    return response
+
 def custom_error(message, status_code):
     response = make_response(jsonify(message), status_code)
     return response

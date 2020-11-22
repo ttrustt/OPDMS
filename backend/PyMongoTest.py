9970d1eec376b8779dc3c094b7f05e18b643d33b
Pattanut Phetmunee <pattanut32@gmail.com>
2020-11-22 16:31:52 +0700

    fix mongofunc

--- backend/PyMongoTest.py
+++ backend/PyMongoTest.py
@@ -5,16 +5,13 @@ def findUDbyPID(pid):
     message = 'error'
     try : 
         client = MongoClient('35.185.182.63', 27018)
-        print(1)
         db = client.opdms
-        print(2)
         collection = db.underlying_disease
-        print(3)
-        x = collection.find({"patient_id":pid})
-        print(x)
-        print(x[0])
-        print(4)
-        message = (True,"Success", x[0]['underlying_disease'])
+        cursor = collection.find({"patient_id":pid})
+        if(len(list(cursor))==0):
+            message = (True,"Not Found",[])
+        else:
+            message = (True,"Success", cursor[0]['underlying_disease'])
     except Exception as e : 
         message =  (False,"Error in findUDbyPID",e)
     client.close()

7573f4a58c1693c4009c160a9a0ad9a1edc8304c
Pattanut Phetmunee <pattanut32@gmail.com>
2020-11-22 16:26:16 +0700

    uncoorect mongofuc

--- backend/PyMongoTest.py
+++ backend/PyMongoTest.py
@@ -1,19 +1,40 @@
 from pymongo import MongoClient
+import pymongo
 
 def findUDbyPID(pid):
-    client = MongoClient('35.185.182.63', 27018)
-    db = client.opdms
-    collection = db.underlying_disease
-    result = []
-    x = collection.find({"patient_id":pid})
-    return x[0]['undertlying_disease']
+    message = 'error'
+    try : 
+        client = MongoClient('35.185.182.63', 27018)
+        print(1)
+        db = client.opdms
+        print(2)
+        collection = db.underlying_disease
+        print(3)
+        x = collection.find({"patient_id":pid})
+        print(x)
+        print(x[0])
+        print(4)
+        message = (True,"Success", x[0]['underlying_disease'])
+    except Exception as e : 
+        message =  (False,"Error in findUDbyPID",e)
+    client.close()
+    return message
 
 def findExbyDID(did):
-    client = MongoClient('35.185.182.63', 27018)
-    db = client.opdms
-    collection = db.expertise
-    x = collection.find({"doctor_id":did})
-    return x[0]['expertise']
+    message = 'error'
+    try : 
+        client = MongoClient('35.185.182.63', 27018)
+        db = client.opdms
+        collection = db.expertise
+        try:
+            x = collection.find({"doctor_id":did})
+            message = (True,"Success",x[0]['expertise'])
+        except Exception as e :
+            message = (True, "Not found",[])
+    except Exception as e :
+        message = (False,"Error in findExbyDID",e)
+    client.close()
+    return message  
 
 def findFMbyPID(pid):
     client = MongoClient('35.185.182.63', 27018)
@@ -23,4 +44,4 @@ def findFMbyPID(pid):
     x = collection.find({"patient_id":pid})
     return x[0]['fm_limitation']
 
-print(findFMbyPID(5))
+print(findUDbyPID(0))
